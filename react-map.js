8bff2abe3d80335fcf9cbbf1cbe4c8af166339f4
Kevin Isom <kevin.isom@gmail.com>
2014-10-01 13:02:03 +1300

    moved react to js file

---
+++ react-map.js
@@ -0,0 +1,28 @@
+/** @jsx React.DOM */
+
+var width = 480,
+    height = 580;
+
+var projection = d3.geo.mercator()
+    .scale(2500)
+    .center([174.77,-41.28])
+    .translate([width / 2, height / 2]);
+
+var path = d3.geo.path()
+    .projection(projection); 
+
+var NzMap = React.createClass({displayName: 'NzMap',
+  render: function() {
+    console.log()
+
+    var nz = topojson.feature(this.props.data, this.props.data.objects.subunits);
+    return this.transferPropsTo(React.DOM.svg(null, 
+      React.DOM.path({d: path(nz)})
+    ));
+  }
+});
+ 
+d3.json("nz.json", function(error, nz) {
+    React.renderComponent(NzMap({width: "480", height: "580", data: nz}), 
+      document.getElelemtById('map'));
+});
